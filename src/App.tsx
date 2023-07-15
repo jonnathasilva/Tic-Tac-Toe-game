a6a7b7e7e6a44f0c5ca2cae874fae3df52eef51f
Jonnathan Silva <sjonnathan928@gmail.com>
2023-07-15 18:04:41 -0300

    feat: restart the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const App = () => {
   const [player, setPlayer] = useState<number>(1);
+  const [isGain, setisGain] = useState<boolean>(false);
   const [List, setList] = useState<string[]>(Array(9).fill(""));
 
   const handClick = (e: React.MouseEvent<HTMLElement>) => {
-    if (List[Number(e.currentTarget.id)] !== "") return;
-    if (checkWinner()) return;
+    if (List[Number(e.currentTarget.id)] !== "" || isGain) return;
 
-    List.splice(Number(e.currentTarget.id), 1, player === 1 ? "X" : "O");
+    console.log(player);
+    List.splice(Number(e.currentTarget.id), 1, player === 1 ? "O" : "X");
     setList([...List]);
+  };
 
-    console.log(Array(9).fill(""));
-
-    player === 1 ? setPlayer(2) : setPlayer(1);
+  const resetGame = () => {
+    setList(Array(9).fill(""));
+    setisGain(false);
+    setPlayer(1);
   };
 
   const checkWinner = () => {
-    let isGain = false;
     const possibleWaysToWin = [
       [List[0], List[1], List[2]],
       [List[3], List[4], List[5]],
@@ -34,17 +36,21 @@ export const App = () => {
     possibleWaysToWin.forEach((cells) => {
       if (cells.every((cell) => cell === "X")) {
         console.log("Jogador 1 vencel!");
-        isGain = true;
+        return setisGain(true);
       }
 
       if (cells.every((cell) => cell === "O")) {
         console.log("Jogador 2 vencel!");
-        isGain = true;
+        return setisGain(true);
       }
     });
 
-    return isGain;
+    player === 1 ? setPlayer(2) : setPlayer(1);
   };
+
+  useEffect(() => {
+    checkWinner();
+  }, [List]);
 
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen space-y-4 bg-gray-300">
@@ -63,7 +69,12 @@ export const App = () => {
         ))}
       </div>
 
-      <button className="capitalize bg-slate-900 text-white px-4 py-2 rounded">
+      {isGain ? <p>Jogagor {player} ganhou!</p> : null}
+
+      <button
+        className="capitalize bg-slate-900 text-white px-4 py-2 rounded"
+        onClick={resetGame}
+      >
         reiniciar
       </button>
     </div>
